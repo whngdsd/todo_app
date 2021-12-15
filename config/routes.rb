c26e62cb3246ee7d9e180caee5e1115c461068d9
whngdsd <win123ora@gmail.com>
2021-12-15 14:30:16 +0900

    Add users new screen

--- config/routes.rb
+++ config/routes.rb
@@ -1,7 +1,9 @@
 Rails.application.routes.draw do
+  get 'users/new'   => "users#new"
   # For details on the DSL available within this file, 
   # see https://guides.rubyonrails.org/routing.html
   root "home#top"
 
   get "tasks/index" => "tasks#index"
+
 end
