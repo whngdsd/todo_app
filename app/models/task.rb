54145631ab655859fcbeec359461234774450e42
whngdsd <win123ora@gmail.com>
2022-01-26 14:40:15 +0900

    Add error messages

--- app/models/task.rb
+++ app/models/task.rb
@@ -2,6 +2,7 @@ class Task < ApplicationRecord
   belongs_to :user
   validates :user_id, presence: true
   validates :name,    presence: true, length: { maximum: 140}
+  validates :scheduled_on, presence: true
   attribute :scheduled_on, :date, default: Date.today
 
   def user
